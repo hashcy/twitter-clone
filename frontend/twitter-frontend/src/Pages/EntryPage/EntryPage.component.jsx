968d6338a9c22643a20436e7009490bdbf5fe40b
hdrock03 <hardik.1rn16ec036@gmail.com>
2022-01-22 15:48:59 +0530

    test code

--- frontend/twitter-frontend/src/Pages/EntryPage/EntryPage.component.jsx
+++ frontend/twitter-frontend/src/Pages/EntryPage/EntryPage.component.jsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 const EntryPage = () => {
     return (
         <Container>
-
+        
             
         </Container>
     )
@@ -13,4 +13,19 @@ export default EntryPage;
 
 /* ---------------------------- STYLED COMPONENTS --------------------------- */
 
-const Container = styled.div``
+const Container = styled.div`
+    
+
+`
+
+
+// const Container = styled.div`
+//     .box {
+
+        
+//         .box-1 {
+
+//         }
+//     }
+
+// `
