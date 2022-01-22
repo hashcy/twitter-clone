3ae798b37dc4a060456a4bb451589e1a56314b3d
Harshvardhan <67060748+HarshVardhan22@users.noreply.github.com>
2022-01-22 15:21:09 +0530

    created homepage

---
+++ frontend/twitter-frontend/src/Pages/EntryPage/EntryPage.component.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const EntryPage = () => {
+    return (
+        <Container>
+
+            
+        </Container>
+    )
+}
+
+export default EntryPage;
+
+/* ---------------------------- STYLED COMPONENTS --------------------------- */
+
+const Container = styled.div``
